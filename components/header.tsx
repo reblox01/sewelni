5c2257d628a6cab5e147736768de1992f6230264
0x8D <mirocairo15@gmail.com>
2024-06-07 21:09:54 +0000

    update ui

--- components/header.tsx
+++ components/header.tsx
@@ -29,8 +29,8 @@ async function UserOrLogin() {
         </>
       ) : (
         <Link href="/new" rel="nofollow">
-          <IconNextChat className="size-6 mr-2 dark:hidden" inverted />
-          <IconNextChat className="hidden size-6 mr-2 dark:block" />
+          <IconVercel className="size-6 mr-2 dark:hidden" inverted />
+          <IconVercel className="hidden size-6 mr-2 dark:block" />
         </Link>
       )}
       <div className="flex items-center">
